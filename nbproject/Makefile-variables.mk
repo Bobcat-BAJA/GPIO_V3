8d63baacda4f125f3b1ef2cd8886b6c511c81bed
lelyons <lelyons@LELYONS-LT2.ad.geigerbrothers.com>
2024-09-18 18:05:30 -0400

    09/08/24-02-LL

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=BAJA_GPIO_R5.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BAJA_GPIO_R5.X.production.hex
